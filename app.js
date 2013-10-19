f80089b5095a4c720b0639b5a93f926e3edf3ae8
Michael Herman <hermanmu@gmail.com>
2013-10-19 13:25:23 -0600

    added images

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-// server side
+/////////////////////////////
+////    server side     ////
+///////////////////////////
 
-
-var express = require('express');
 // var routes = require('./routes');
 // var user = require('./routes/user');
+
+// dependencies
+var express = require('express');
 var http = require('http');
 var path = require('path');
 var request = require('request');
@@ -25,29 +28,41 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// first route
 app.get('/', function(req, res) {res.render('index')});
 
+// second route
 app.get('/searching', function(req, res){
 
+	// input value from search
 	var val = req.query.search;
+	//console.log(val);
 
+	// url used to search yql
 	var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20craigslist.search" +
 	"%20where%20location%3D%22sfbay%22%20and%20type%3D%22jjj%22%20and%20query%3D%22" + val + "%22&format=" +
 	"json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+	console.log(url);
 
+	// request module is used to process the yql url and return the results in JSON format
 	request(url, function(err, resp, body) {
-		body = JSON.parse(body)
+		body = JSON.parse(body);
+		// logic used to compare search results with the input from user
 		if (body.query.count === 0 || typeof body.query.results.RDF.item === 'undefined') {
-		   craig = "No results found. Try again." 
+		   craig = "No results found. Try again.";
 		} else {
-	     craig = body.query.results.RDF.item[0]['about']
-	     console.log(craig)
+	     craig = body.query.results.RDF.item[0]['about'];
 	  }
+	  // pass back the results to client side
 		res.send(craig);
-	})
+	});
 
-})
+	//testing the route
+	// res.send("WHEEE");
 
+});
+
+// old routes
 // app.get('/', routes.index);
 // app.get('/users', user.list);
 
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,15 @@
-// client side
+/////////////////////////////
+////    client side     ////
+///////////////////////////
+
 
 $(function(){
   $('#search').on('keyup', function(e){
-  if(e.keyCode === 13) {
-    var parameters = { search: $(this).val() };
-    $.get( '/searching',parameters, function(data) {
-        $('#results').html(data);
-      })
-  }
-  })
-})
+	  if(e.keyCode === 13) {
+	    var parameters = { search: $(this).val() };
+	    $.get( '/searching',parameters, function(data) {
+	      $('#results').html(data);
+	    });
+  	};
+  });
+});
