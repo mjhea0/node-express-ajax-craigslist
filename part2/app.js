94d28b69afed38a670f928ce0ad88b01be4b71ac
Michael Herman <hermanmu@gmail.com>
2013-10-28 14:44:10 -0600

    updates

--- part2/app.js
+++ part2/app.js
@@ -10,7 +10,7 @@ var request = require('request');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3003);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -36,14 +36,23 @@ app.get('/searching', function(req, res){
 	var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20craigslist.search" +
 	"%20where%20location%3D%22sfbay%22%20and%20type%3D%22jjj%22%20and%20query%3D%22" + val + "%22&format=" +
 	"json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+	
+	requests(url,function(data){
+		res.send(data);
+	});
+});
+
+
+function requests(url, callback) {
 	// request module is used to process the yql url and return the results in JSON format
 	request(url, function(err, resp, body) {
-		resultsArray = [];
+		var resultsArray = [];
 		body = JSON.parse(body);
+		// console.log(body.query.results.RDF.item)
 		// logic used to compare search results with the input from user
-		if (!body.query.results.RDF.item['about'] === false) {
+		if (typeof body.query.results.RDF.item === "undefined") {
 		  results = "No results found. Try again.";
-		  res.send(results)
+		  callback(results);
 		} else {
 			results = body.query.results.RDF.item;
 			for (var i = 0; i < results.length; i++) {
@@ -53,9 +62,9 @@ app.get('/searching', function(req, res){
 			};
 		};
 	  // pass back the results to client side
-	  res.send(resultsArray);
+	  callback(resultsArray);
 	});
-});
+};
 
 // run server
 http.createServer(app).listen(app.get('port'), function(){
