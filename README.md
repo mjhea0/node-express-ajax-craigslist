89813a047a278b881ef52311df6613ae40a3de26
Michael Herman <hermanmu@gmail.com>
2014-04-15 13:05:37 -0600

    part4

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ check out the blog posts -
 1. http://mherman.org/blog/2013/10/20/handling-ajax-calls-with-node-dot-js-and-express-scraping-craigslist/
 2. http://mherman.org/blog/2013/11/01/handling-ajax-calls-with-node-dot-js-and-express-part-2/
 3. http://mherman.org/blog/2013/12/21/handling-ajax-calls-with-node-dot-js-and-express-part-3/
+4. http://mherman.org/blog/2014/04/15/handling-ajax-calls-with-node-dot-js-and-express-part-4/
--- part4/app.js
+++ part4/app.js
@@ -7,59 +7,14 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var mongoose = require('mongoose')
-var passport = require('passport')
-var GoogleStrategy = require('passport-google').Strategy;
 var config = require('./config');
-var user = require('./models/user');
 var routes = require('./routes');
+var passport = require('./authentication');
 
 var app = express();
 
 // connect to the database
 mongoose.connect(config.mongoUrl);
-
-// passport settings
-passport.serializeUser(function(user, done) {
-  console.log('serializeUser: ' + user.id)
-  done(null, user.id);
-});
-passport.deserializeUser(function(id, done) {
-  user.findOne({_id : id}, function(err, user) {
-    console.log(user)
-    if(!err) done(null, user);
-    else done(err, null)
-  });
-});
-
-passport.use(new GoogleStrategy({
-  returnURL: config.google.returnURL,
-  realm: config.google.realm
-},
-  function(identifier, profile, done) {
-    console.log(profile.emails[0].value)
-    process.nextTick(function() {
-      var query = user.findOne({'email': profile.emails[0].value});
-      query.exec(function(err, oldUser) {
-        if(oldUser) {
-          console.log("Found registered user: " + oldUser.name + " is logged in!");
-          done(null, oldUser);
-        } else {
-          var newUser = new user();
-          newUser.name = profile.displayName;
-          newUser.email = profile.emails[0].value;
-          console.log(newUser);
-          newUser.save(function(err){
-            if(err){
-              throw err;
-            }
-            console.log("New user, " + newUser.name + ", was created");
-            done(null, newUser);
-          });
-        }
-      });
-    });
-  }
-));
 
 // all environments
 app.set('port', process.env.PORT || 3000);
